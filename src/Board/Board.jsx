bedfd514c6b0391eb3645dc474e01376a79ed4dc
Huseyn <huseyn.mikayil95@gmail.com>
2020-12-29 12:30:30 +0400

    Implement setRandomValue functionality

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -5,27 +5,46 @@ import "./Board.css";
 class Board extends Component {
   state = {
     squares: [
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
-      { displayValue: "" },
+      { id: 1, displayValue: "" },
+      { id: 2, displayValue: "" },
+      { id: 3, displayValue: "" },
+      { id: 4, displayValue: "" },
+      { id: 5, displayValue: "" },
+      { id: 6, displayValue: "" },
+      { id: 7, displayValue: "" },
+      { id: 8, displayValue: "" },
+      { id: 9, displayValue: "" },
+      { id: 10, displayValue: "" },
+      { id: 11, displayValue: "" },
+      { id: 12, displayValue: "" },
+      { id: 13, displayValue: "" },
+      { id: 14, displayValue: "" },
+      { id: 15, displayValue: "" },
+      { id: 16, displayValue: "" },
     ],
   };
 
   setRandomValue = () => {
-    console.log("Set Random Value");
+    let { squares } = this.state;
+    const index1 = Math.floor(Math.random() * squares.length);
+    squares[index1] = this.updateDisplayValue(squares[index1], "2");
+
+    let index2 = Math.floor(Math.random() * squares.length);
+    while (index1 === index2)
+      index2 = Math.floor(Math.random() * squares.length);
+
+    squares[index2] = this.updateDisplayValue(squares[index2], "2");
+
+    this.setState({ squares });
+
+    console.log(squares);
+  };
+
+  updateDisplayValue = (obj, value) => {
+    return {
+      ...obj,
+      displayValue: value,
+    };
   };
 
   moveRight = () => {
@@ -43,6 +62,19 @@ class Board extends Component {
   moveDown = () => {
     console.log("Move Down");
   };
+
+  executeFunction(event) {
+    if (event.keyCode === 27) {
+      //Do whatever when esc is pressed
+    }
+  }
+  componentDidMount() {
+    this.setRandomValue();
+    document.addEventListener("keydown", this.executeFunction, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.executeFunction, false);
+  }
 
   render = () => {
     const squares = this.state.squares.map((square) => (
--- src/Board/Board.test.js
+++ src/Board/Board.test.js
@@ -4,23 +4,38 @@ import Board from "./Board";
 
 describe("Board", () => {
   let wrapper;
+  let squares;
 
-  beforeEach(() => (wrapper = shallow(<Board />)));
+  beforeEach(() => {
+    wrapper = shallow(<Board />);
+    squares = [
+      { id: 1, displayValue: "" },
+      { id: 2, displayValue: "" },
+      { id: 3, displayValue: "" },
+      { id: 4, displayValue: "" },
+    ];
+  });
 
-  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+  it("should render correctly", () => expect(wrapper).toMatchSnapshot());
 
   it("should render a div", () => {
     expect(wrapper.find("div").length).toEqual(1);
   });
 
   it("should render an instance of the Square component for each square", () => {
-    const squares = [
-      { displayValue: "0" },
-      { displayValue: "1" },
-      { displayValue: "2" },
-    ];
-
     wrapper.setState({ squares });
     expect(wrapper.find("Square").length).toEqual(squares.length);
+  });
+
+  it("setRandomValue", () => {
+    wrapper.setState({ squares });
+    wrapper.instance().setRandomValue();
+    expect(squares.filter((x) => x.displayValue !== "").length).toEqual(2);
+  });
+
+  it("updateDisplayValue", () => {
+    let obj = { id: 1, displayValue: "" };
+    obj = wrapper.instance().updateDisplayValue(obj, "2");
+    expect(obj).toMatchObject({ id: 1, displayValue: "2" });
   });
 });
